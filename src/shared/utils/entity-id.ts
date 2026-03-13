dd5192e36a04a6c74ccebcc3ef8f5ef9039f73f5
michota <109215822+Michota@users.noreply.github.com>
2026-03-13 19:14:59 +0100

    minor

--- src/shared/utils/entity-id.ts
+++ src/shared/utils/entity-id.ts
@@ -2,5 +2,10 @@ import { randomUUID, UUID } from 'crypto';
 
 export abstract class EntityId<T extends string> {
     declare private readonly _brand: T;
-    constructor(readonly value: UUID = randomUUID()) {}
+
+    generate(): UUID {
+        return randomUUID();
+    }
+
+    constructor(readonly value: UUID = this.generate()) {}
 }
