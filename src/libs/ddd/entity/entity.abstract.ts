301e143bcb661c5acee860e3755952824afefbf1
michota <109215822+Michota@users.noreply.github.com>
2026-03-14 10:15:37 +0100

    feat: create entity getters and setters

--- src/libs/ddd/entity/entity.abstract.ts
+++ src/libs/ddd/entity/entity.abstract.ts
@@ -1,13 +1,29 @@
 type EntityId<T> = string & { __brand: T };
 
 export abstract class Entity<T, Id = EntityId<T>> {
-    id: Id;
-    createdAt: Date;
-    updatedAt?: Date;
+    private _id: Id;
+    private _createdAt: Date;
+    private _updatedAt?: Date;
 
     constructor(id: Id, createdAt: Date, updatedAt?: Date) {
-        this.id = id;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
+        this._id = id;
+        this._createdAt = createdAt;
+        this._updatedAt = updatedAt;
+    }
+
+    get id(): Id {
+        return this._id;
+    }
+
+    private set id(newId: Id) {
+        this._id = newId;
+    }
+
+    get createdAt(): Date {
+        return this._createdAt;
+    }
+
+    get updatedAt(): Date | undefined {
+        return this._updatedAt;
     }
 }
