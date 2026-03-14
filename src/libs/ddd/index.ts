d1b55251deec7adbdf53909647288eb9a5212543
michota <109215822+Michota@users.noreply.github.com>
2026-03-14 20:30:01 +0100

    feat: add DDD helper exports for AggregateRoot, Command, DomainEvent, Entity, and ValueObject

---
+++ src/libs/ddd/index.ts
@@ -0,0 +1,5 @@
+export { AggregateRoot } from "./aggregate-root.abstract";
+export { Command } from "./command";
+export { DomainEvent, type DomainEventProperties } from "./domain-event.abstract";
+export { Entity, type EntityProps } from "./entity.abstract";
+export { ValueObject } from "./value-objects";
