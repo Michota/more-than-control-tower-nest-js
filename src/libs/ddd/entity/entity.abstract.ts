777e1d3e21745bcdefd30544fac81ecac4f8b027
michota <109215822+Michota@users.noreply.github.com>
2026-03-14 10:30:31 +0100

    feat: add isEntity and equals methods

--- src/libs/ddd/entity/entity.abstract.ts
+++ src/libs/ddd/entity/entity.abstract.ts
@@ -1,21 +1,30 @@
 type EntityId<T> = string & { __brand: T };
 
-export abstract class Entity<T, Id = EntityId<T>> {
-    private _id: Id;
+export interface BaseEntityProps<T> {
+    id: EntityId<T>;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+export interface CreateEntityProps<T> extends BaseEntityProps<T> {}
+
+export abstract class Entity<T> {
+    private _id: EntityId<T>;
     private _createdAt: Date;
     private _updatedAt?: Date;
 
-    constructor(id: Id, createdAt: Date, updatedAt?: Date) {
+    constructor({ id, createdAt, updatedAt }: CreateEntityProps<T>) {
         this._id = id;
-        this._createdAt = createdAt;
+        this._createdAt = createdAt || new Date();
         this._updatedAt = updatedAt;
     }
 
-    get id(): Id {
+    get id(): EntityId<T> {
         return this._id;
     }
 
-    private set id(newId: Id) {
+    private set id(newId: EntityId<T>) {
         this._id = newId;
     }
 
@@ -25,5 +34,29 @@ export abstract class Entity<T, Id = EntityId<T>> {
 
     get updatedAt(): Date | undefined {
         return this._updatedAt;
+    }
+
+    static isEntity(entity: unknown): entity is Entity<unknown> {
+        return entity instanceof Entity;
+    }
+
+    /**
+     * Checks if two entities are the same Entity by comparing ID field.
+     * @param object Entity
+     */
+    public equals(object?: Entity<T>): boolean {
+        if (object === null || object === undefined) {
+            return false;
+        }
+
+        if (this === object) {
+            return true;
+        }
+
+        if (!Entity.isEntity(object)) {
+            return false;
+        }
+
+        return this.id ? this.id === object.id : false;
     }
 }
