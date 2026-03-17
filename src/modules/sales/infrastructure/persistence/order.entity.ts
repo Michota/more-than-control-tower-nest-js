4e29c7da87bd7a71b962055a8c2c5bc88979ba5d
michota <109215822+Michota@users.noreply.github.com>
2026-03-17 11:08:08 +0100

    feat: improve order schema by adding customerId

--- src/modules/sales/infrastructure/persistence/order.entity.ts
+++ src/modules/sales/infrastructure/persistence/order.entity.ts
@@ -1,18 +1,23 @@
 import { defineEntity, p } from "@mikro-orm/core";
 import { OrderStatus } from "../../domain/order.status";
 import { OrderLine } from "./order-line.embeddable";
+import { currency } from "@src/shared/persistence/currency.property";
 
-const Order = defineEntity({
+const OrderSchema = defineEntity({
     name: "Order",
     tableName: "orders",
     properties: {
         id: p.uuid().primary(),
         cost: p.decimal().nullable(),
-        costCurrency: p.string().length(3).nullable(),
+        currency,
         status: p.enum(() => OrderStatus),
         orderLines: p.embedded(OrderLine).array().default([]),
-        customer: p.json(),
+        customerId: p.uuid,
     },
 });
+
+class Order extends OrderSchema.class {}
+
+OrderSchema.setClass(Order);
 
 export { Order };
