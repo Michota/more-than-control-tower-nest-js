77f5365b26ec781799170e847927314b2025288e
michota <109215822+Michota@users.noreply.github.com>
2026-03-18 13:37:18 +0100

    feat: ValueObjectWithSchema

---
+++ src/shared/ddd/value-object-with-schema.abstract.ts
@@ -0,0 +1,16 @@
+import { ValueObject } from "@src/libs/ddd";
+import z, { ZodType } from "zod";
+
+export abstract class ValueObjectWithSchema<
+    S extends ZodType,
+    T extends z.infer<S> = z.infer<S>,
+> extends ValueObject<T> {
+    protected abstract schema: S;
+
+    /**
+     * This method should be called in the constructor of the child class after setting the properties, to validate the properties against the schema.
+     */
+    validate(): void {
+        this.schema.parse(this.properties);
+    }
+}
